650b0ea9cdbdda19e2ea32a71913be33bda23f01
czhiyuan <zhiyuan20210520@163.com>
2023-03-31 10:07:33 +0800

    fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -3,7 +3,7 @@
 
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
-
+//11
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'chenzhiyuan_blog',
